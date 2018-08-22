9e07fe572333a6726d27d98f00ce5b21ea832d42
Kautilya <kautilyasave@gmail.com>
2018-08-22 23:21:56 +0530

    Easter Egg.

---
+++ js/s/k/1.js
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	
+</head>
+
+<body>
+<H1>
+You shouldn't be Here .
+This site is not affiliated with the Main site.
+</h1>
+<object data=https://reddpics.com/r/ProgrammerHumor/ width="1366" height="768"> <embed src=https://reddpics.com/r/ProgrammerHumor/  width="1366" height="768""> </embed> Error: Embedded data could not be displayed. </object>
+
+
+</body>
+</html>
